1ed4228225481f3ab67b5837dfb2b29e42343d34
Muhannes <haneda-3@student.ltu.se>
2017-02-27 14:34:14 +0100

    Initial commit 2

---
+++ python/DataCluster.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+
+
+class DataCluster(object):
+    def __init__(self):
+        pass
+
+    def train(self, X, y):
+        self.Xtr = X
+        self.ytr = y
+        
+
+        
+    
