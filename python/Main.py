7a6f026200a4fcd78da5097081fee5e2367f678e
Muhannes <haneda-3@student.ltu.se>
2017-03-03 11:11:43 +0100

    tests for all users

--- python/Main.py
+++ python/Main.py
@@ -8,32 +8,37 @@ def Main():
     dataReader = DataReader()
     allUserData = dataReader.loadData("DSL-StrongPasswordData") #loads all users data
     
+    classifier = Classifier()
     
-    
-    # second person for comparison
-    owner_index = 0
-    first_time = True
-    for i in range(0,50):
-        userDataRaw = allUserData[i] #data from 1 user
-        userData = dataReader.formatData(userDataRaw) #formats data (strips user and session ids etc), returns Matrix.
-        if i == owner_index:
-            print "person1 created!"
-            person1 = DataCluster(userData[0:300])
-            test_data_right = userData[300:]
-            print test_data_right
-        else:
-            if first_time:
-                test_data_wrong = userData
-                first_time = False
+    for k in range(0,50):
+        correct_person_accuracy = []
+        wrong_person_accuracy = []
+        owner_index = k # index for the user that is to be tested
+        first_time = True # temp variable for checking if first time creating test_data_wrong
+        for i in range(0,50):
+            userDataRaw = allUserData[i] #data from 1 user
+            userData = dataReader.formatData(userDataRaw) #formats data (strips user and session ids etc), returns Matrix.
+            if i == owner_index:
+                print "person1 created!"
+                np.random.shuffle(userData) # Shuffle to get data from different sessions
+                person1 = DataCluster(userData[0:300]) # creates the person to be tested
+                test_data_right = userData[300:]
+                # print test_data_right
             else:
-                test_data_wrong = np.concatenate((test_data_wrong,userData), axis=0)
+                if first_time:
+                    test_data_wrong = userData
+                    first_time = False
+                else:
+                    test_data_wrong = np.concatenate((test_data_wrong,userData), axis=0)
+        correct_person_accuracy.append(classifier.compare_all(person1, test_data_right, True))
+        wrong_person_accuracy.append(classifier.compare_all(person1, test_data_wrong, False))
             
     
     
-    classifier = Classifier()
     
-    print "correct person: " + str(classifier.compare_all(person1, test_data_right, True))
-    print "wrong person: " + str(classifier.compare_all(person1, test_data_wrong, False))
+    
+    print "correct person: " + str(np.mean(correct_person_accuracy))
+    print "wrong person: " + str(np.mean(wrong_person_accuracy))
     
     
 Main()
